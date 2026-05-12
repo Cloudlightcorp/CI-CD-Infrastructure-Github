317fb3c323eeedfa8ceb8d9c9890ff6942fe4f39
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2026-05-12 11:46:44 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+############################################################
+# TERRAFORM BACKEND
+############################################################
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-codepipeline-shared-bucket"
+    key    = "terraform/state.tfstate"
+    region = "us-west-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+############################################################
+# PROVIDER
+############################################################
+
+provider "aws" {
+  region = var.region
+}
