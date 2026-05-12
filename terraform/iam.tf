722456e746fe6748f1fb2ddcb9ef95119b8a3d0c
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2026-05-12 11:46:32 +0530

    Create iam.tf

---
+++ terraform/iam.tf
@@ -0,0 +1,96 @@
+############################################################
+# ECS EXECUTION ROLE
+############################################################
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole-git-onlinemobilestore"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+############################################################
+# CODEBUILD ROLE
+############################################################
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-git-onlinemobilestore-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:*",
+          "logs:*",
+          "s3:*",
+          "ecs:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+############################################################
+# CODEPIPELINE ROLE
+############################################################
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-git-onlinemobilestore-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
