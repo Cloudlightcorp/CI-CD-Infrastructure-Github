16177a02e1281bbb28f7b169b9231ab1dd9e2c20
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2026-05-12 11:42:04 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,23 @@
+############################################################
+# SECURITY GROUP
+############################################################
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "GIT-OnlineMobileStore-SG"
+  description = "Allow HTTP traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
